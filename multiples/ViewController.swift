41e037f807226c363874ed6fce47a57057066c48
Ben Graham <bengraham@Bens-MacBook-Pro.local>
2016-05-01 13:56:23 +1000

    finished multiples

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var multiplesLogoImg: UIImageView!
+    @IBOutlet weak var calculationLbl: UILabel!
+    @IBOutlet weak var numberToMultiplyTxt: UITextField!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var playBtn: UIButton!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Variables
+    var numA = 0
+    var maxGames = 10
+    var numGames = 0
+    
+    @IBAction func playGame(sender: UIButton) {
+        if numberToMultiplyTxt.text != nil && numberToMultiplyTxt.text != "" {
+            multiplesLogoImg.hidden = true
+            numberToMultiplyTxt.hidden = true
+            playBtn.hidden = true
+            calculationLbl.hidden = false
+            addBtn.hidden = false
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addMultiplier(sender: UIButton) {
+        numGames+=1
+        if (numGames <= maxGames){
+            let numB: Int = Int(numberToMultiplyTxt.text!)!
+            calculationLbl.text = "\(numA) + \(numB) = " + String(numA + numB)
+            numA+=numB
+        } else {
+            //reset game
+            multiplesLogoImg.hidden = false
+            numberToMultiplyTxt.hidden = false
+            playBtn.hidden = false
+            calculationLbl.hidden = true
+            addBtn.hidden = true
+            numGames = 0
+            numA = 0
+            numberToMultiplyTxt.text = ""
+            calculationLbl.text = "Press Add to add!"
+        }
     }
-
 
 }
 
